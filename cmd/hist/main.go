bcc035e7fc3570f1d258a0c21a0fb520233cdd37
Rafal Jeczalik <rjeczalik@gmail.com>
2015-02-04 17:22:42 +0100

    cmd/hist: add -slice flag

--- cmd/hist/main.go
+++ cmd/hist/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -9,6 +11,13 @@ import (
 	"strconv"
 	"strings"
 )
+
+func min(i, j int) int {
+	if i > j {
+		return j
+	}
+	return i
+}
 
 type pair struct {
 	s string
@@ -25,19 +34,16 @@ func (b byvalue) Search(s string) int {
 	return sort.Search(len(b), func(i int) bool { return b[i].s >= s })
 }
 
-func (b *byvalue) Add(s string) int {
+func (b *byvalue) Add(s string) {
 	switch i := b.Search(s); {
 	case i == len(*b):
 		*b = append(*b, pair{s: s, n: 1})
-		return 1
 	case (*b)[i].s == s:
 		(*b)[i].n++
-		return (*b)[i].n
 	default:
 		*b = append(*b, pair{})
 		copy((*b)[i+1:], (*b)[i:])
 		(*b)[i] = pair{s: s, n: 1}
-		return 1
 	}
 }
 
@@ -48,17 +54,75 @@ func (b bycount) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
 func (b bycount) Less(i, j int) bool { return b[i].n >= b[j].n }
 func (b bycount) Sort()              { sort.Sort(b) }
 
+var errSyntax = errors.New("invalid range value syntax")
+
+type sliceVar [2]int
+
+func (s *sliceVar) Set(str string) error {
+	m := strings.Split(str, ":")
+	if len(m) != 2 {
+		return errSyntax
+	}
+	switch {
+	case m[0] == "" && m[1] == "":
+		*s = [2]int{0, -1}
+	case m[0] == "":
+		n, err := strconv.Atoi(m[1])
+		if err != nil {
+			return err
+		}
+		*s = [2]int{0, n}
+	case m[1] == "" || m[1] == "-1":
+		n, err := strconv.Atoi(m[0])
+		if err != nil {
+			return err
+		}
+		*s = [2]int{n, -1}
+	default:
+		n, err := strconv.Atoi(m[0])
+		if err != nil {
+			return err
+		}
+		o, err := strconv.Atoi(m[1])
+		if err != nil {
+			return err
+		}
+		*s = [2]int{n, o}
+	}
+	return nil
+}
+
+func (s sliceVar) String() string {
+	switch {
+	case s == [2]int{0, -1}:
+		return ":"
+	case s[0] == 0:
+		return ":" + strconv.Itoa(s[1])
+	case s[1] == -1:
+		return strconv.Itoa(s[0]) + ":"
+	default:
+		return strconv.Itoa(s[0]) + ":" + strconv.Itoa(s[1])
+	}
+}
+
+var slice = sliceVar{0, -1}
+
 func die(v interface{}) {
 	fmt.Fprintln(os.Stderr, v)
 	os.Exit(1)
 }
 
+func init() {
+	flag.Var(&slice, "slice", "limit result set using given slice indices")
+	flag.Parse()
+}
+
 func main() {
 	var r io.Reader = os.Stdin
-	switch len(os.Args) {
+	switch flag.NArg() {
+	case 0:
 	case 1:
-	case 2:
-		f, err := os.Open(os.Args[1])
+		f, err := os.Open(flag.Arg(0))
 		if err != nil {
 			die(err)
 		}
@@ -71,25 +135,35 @@ func main() {
 	hist := byvalue{}
 	s := bufio.NewScanner(r)
 	for s.Scan() {
-		if n := hist.Add(s.Text()); n > max {
+		s := s.Text()
+		if n := len(s); n > max {
 			max = n
 		}
+		hist.Add(s)
 	}
 	if err := s.Err(); err != nil {
 		die(err)
 	}
+	bycount(hist).Sort()
+	// Slice the result set.
+	//
+	// TODO(rjeczalik): update max
+	n := len(hist)
+	if slice[1] == -1 {
+		slice[1] = n
+	}
+	hist = hist[min(slice[0], n):min(slice[1], n)]
 	if len(hist) == 0 {
 		return
 	}
-	bycount(hist).Sort()
-	format := "%" + strconv.Itoa(len(strconv.Itoa(max))) + "s\t%d\t"
-	n, _ := fmt.Printf(format, hist[0].s, hist[0].n)
+	format := "%" + strconv.Itoa(max+1) + "s\t%d\t"
+	n, _ = fmt.Printf(format, hist[0].s, hist[0].n)
 	n = 70 - n
-	fmt.Println(strings.Repeat("#", n))
+	fmt.Println(strings.Repeat("░", n))
 	for i := 1; i < len(hist); i++ {
 		fmt.Printf(format, hist[i].s, hist[i].n)
-		if r := n * hist[i].n / max; r != 0 {
-			fmt.Print(strings.Repeat("#", r))
+		if r := n * hist[i].n / hist[0].n; r != 0 {
+			fmt.Print(strings.Repeat("░", r))
 		}
 		fmt.Println()
 	}
